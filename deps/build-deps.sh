c4414fe62ec920a39d119050592119a351f58b50
Brett Randall <javabrett@gmail.com>
2017-12-01 10:30:01 +1100

    Run without command-chaining, and with set -e.

--- deps/build-deps.sh
+++ deps/build-deps.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 PWD=$(pwd)
 TMUX_SRC_DIR=${PWD}/tmux-2.6
 LIBEVENT_SRC_DIR=${PWD}/libevent-2.1.8-stable
@@ -11,7 +13,7 @@ GPM_SRC_DIR=${PWD}/gpm-1.20.7
 
 export INSTALL_DIR=/usr/local/mosh
 export CFLAGS="-I${INSTALL_DIR}/include -fPIC"
-export CXXFLAGS=$CFLAGS
+export CXXFLAGS="${CFLAGS}"
 export PATH="${INSTALL_DIR}/bin:${PATH}"
 export LDFLAGS="-L${INSTALL_DIR}/lib -Wl,-rpath,${INSTALL_DIR}/lib"
 export PKG_CONFIG_PATH="${INSTALL_DIR}/lib/pkgconfig"
@@ -21,30 +23,40 @@ MAKEOPTS=-j4
 
 cd $LIBEVENT_SRC_DIR
 make clean || echo
-./configure --enable-static=yes --enable-shared=yes --prefix=${INSTALL_DIR} && make $MAKEOPTS && make install
+./configure --enable-static=yes --enable-shared=yes --prefix=${INSTALL_DIR}
+make $MAKEOPTS
+make install
 
 cd $OPENSSL_SRC_DIR
 make clean || echo
-./config no-shared -fPIC --prefix=${INSTALL_DIR} && make $MAKEOPTS && make install
-
+./config no-shared -fPIC --prefix=${INSTALL_DIR}
+make $MAKEOPTS
+make install
 
 echo "Building protobuf..."
 cd $PROTOBUF_SRC_DIR
 make clean || echo
-./configure --disable-shared --prefix=${INSTALL_DIR}; make $MAKEOPTS && make install
-
+./configure --disable-shared --prefix=${INSTALL_DIR}
+make $MAKEOPTS
+make install
 
 echo "Building ncursesw..."
 cd $NCURSES_SRC_DIR
 make clean || echo
-./configure --enable-pc-files --enable-widec --without-gpm --with-pkg-config-libdir=${PKG_CONFIG_PATH} --prefix=${INSTALL_DIR} && make $MAKEOPTS &&  make install
+./configure --enable-pc-files --enable-widec --without-gpm --with-pkg-config-libdir=${PKG_CONFIG_PATH} --prefix=${INSTALL_DIR}
+make $MAKEOPTS
+make install
 
 echo "Building ncurses..."
 cd $NCURSES_SRC_DIR
 make clean || echo
-./configure --enable-pc-files --without-gpm --with-pkg-config-libdir=${PKG_CONFIG_PATH} --prefix=${INSTALL_DIR} && make $MAKEOPTS &&  make install
+./configure --enable-pc-files --without-gpm --with-pkg-config-libdir=${PKG_CONFIG_PATH} --prefix=${INSTALL_DIR}
+make $MAKEOPTS
+make install
 
 echo "Building tmux..."
 cd $TMUX_SRC_DIR
 make clean || echo
-./configure --prefix=${INSTALL_DIR} && make $MAKEOPTS && make install
+./configure --prefix=${INSTALL_DIR}
+make $MAKEOPTS
+make install
